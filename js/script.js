6907884c4b40b517ea9d1b38cdf5fb7c208ae314
darshan72 <darshankhairnar72@gmail.com>
2023-04-07 15:17:30 +0530

    completed header and hero section

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Scrolling Effect
+let nav = document.querySelector(".navbar");
+
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20)
+    {
+        nav.classList.add("header-scrolled");
+    }
+    else
+    {
+        nav.classList.remove("header-scrolled");
+    }
+}
